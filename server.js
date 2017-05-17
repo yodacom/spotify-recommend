a909bf57665a7326e43ab423c098163b72ff48af
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-05-17 10:06:26 -0400

    Added async functions to spotify

--- server.js
+++ server.js
@@ -4,23 +4,44 @@ var events = require("events");
 
 var getFromApi = function(endpoint, args){
 	var emitter = new events.EventEmitter();
-	unirest.get("https://api.spotify.com.v1/" + endpoint)
-    .qs(args)
+	unirest.get("https://api.spotify.com/v1/" + endpoint)
+    .qs(args) //?q=u2&limit=1&type=artist
     .end(function(response){
-	if (response.ok) {
-		emitter.emit("end",response.body);
-	}
-	else {
-		emitter.emit("error", response.code);
-	}
-});
+
+		if (response.ok) {
+			emitter.emit("end",response.body);
+		}
+		else {
+			emitter.emit("error", response.code);
+		}
+	});
 	return emitter;
 };
 
 var app = express();
 app.use(express.static("public"));
 
+
+async function getTopTracks(related){
+    return await Promise.all(related.map(function(related){
+
+        return new Promise(function(resolve, reject){
+            var topTracksReq = getFromApi("artists/"+ related.id +"/top-tracks", {
+                country:"US"
+            });
+            topTracksReq.on("end", function(item){
+                related.tracks = item.tracks;
+                console.log(related);
+                resolve(related);
+            });
+        });
+
+    }));
+}
+
+
 app.get("/search/:name", function(req,res){
+    console.log(req.params);
 	var searchReq = getFromApi("search", {
 		q: req.params.name,
 		limit: 1,
@@ -28,8 +49,25 @@ app.get("/search/:name", function(req,res){
 	});
 
 	searchReq.on("end", function(item){
-		var artist = item.artist.items[0];
-		res.json(artist);
+		var artist = item.artists.items[0];
+		//we successfully retrieved an artist
+        //now find related artist
+        var relatedReq = getFromApi("artists/" + artist.id + "/related-artists", {});
+        relatedReq.on("end", function(item){
+            //successful return
+            artist.related = item.artists;
+            getTopTracks(artist.related)
+                .then(function(related){
+                    artist.related = related;
+                    res.json(artist);
+                });
+
+        });
+        relatedReq.on("error", function(code){
+            //error occured
+            res.sendStatus(code);
+        });
+
 	});
 
 	searchReq.on("error", function(code){
